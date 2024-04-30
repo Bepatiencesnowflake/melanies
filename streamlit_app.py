c14b86812c3a610c443c32e4a57521e5a138bf9a
Bepatiencesnowflake <168467747+Bepatiencesnowflake@users.noreply.github.com>
2024-04-29 23:27:51 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+# Import python packages
+import streamlit as st
+import requests
+
+# from snowflake.snowpark.context import get_active_session
+
+# Write directly to the app
+st.title("Customer Your Smoothie :cup_with_straw:")
+st.write("Choose the fruits you want in your custom Smoothie")
